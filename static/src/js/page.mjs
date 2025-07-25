aff14a27f2faa0da30b47aa47a9f35216eaff285
Juan Manuel Lopez <git.nrbye@slmail.me>
2025-07-25 15:27:43 -0300

    Init `rrDetailsTabs` module

--- static/src/js/page.mjs
+++ static/src/js/page.mjs
@@ -1,7 +1,9 @@
 import rrHighlight from './highlight.mjs'
+import rrDetailsTabs from './details-tabs.mjs'
 
 // Run logic after the HTML document is fully loaded and parsed. Ensures all
 // elements are accessible and prevents script execution errors.
 window.addEventListener('DOMContentLoaded', () => {
+    rrDetailsTabs.init('div.details.tabs');
     rrHighlight.init();
 });
