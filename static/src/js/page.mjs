b67abe17e77f5c2623301b45e6b7a65ec424f517
Juan Manuel Lopez <git.nrbye@slmail.me>
2025-07-31 16:23:57 -0300

    Import and init custom `mermaid.js` module

--- static/src/js/page.mjs
+++ static/src/js/page.mjs
@@ -1,9 +1,11 @@
 import rrHighlight from './highlight.mjs'
 import rrDetailsTabs from './details-tabs.mjs'
+import rrMermaid from './mermaid.mjs'
 
 // Run logic after the HTML document is fully loaded and parsed. Ensures all
 // elements are accessible and prevents script execution errors.
 window.addEventListener('DOMContentLoaded', () => {
     rrDetailsTabs.init('div.details.tabs');
+    rrMermaid.init();
     rrHighlight.init();
 });
