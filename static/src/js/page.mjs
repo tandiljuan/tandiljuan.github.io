7ab91b5d8490248af2d94c4ca64e7cafc791c739
Juan Manuel Lopez <git.nrbye@slmail.me>
2025-07-31 16:20:34 -0300

    Import and init custom `highlight.js` module

--- static/src/js/page.mjs
+++ static/src/js/page.mjs
@@ -1,2 +1,7 @@
+import rrHighlight from './highlight.mjs'
+
+// Run logic after the HTML document is fully loaded and parsed. Ensures all
+// elements are accessible and prevents script execution errors.
 window.addEventListener('DOMContentLoaded', () => {
+    rrHighlight.init();
 });
